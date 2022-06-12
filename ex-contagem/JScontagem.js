815fecd8a3e1bb7fa8569b30dc99123da26733a1
devluisfelipe <104323220+devluisfelipe@users.noreply.github.com>
2022-06-12 12:23:40 -0300

    atualização nome pastas - 12.06.22

--- ex-contagem/JScontagem.js
+++ ex-contagem/JScontagem.js
@@ -7,7 +7,7 @@ function clicar () {
 
     var ans = window.document.getElementById('resposta')
 
-    if (inic == fim || inic == 0 || fim == 0 || espaç == 0) {    /* igual a zero também significa que o campo está vazio ou sem valor */
+    if (inic == fim || inic == 0 || fim == 0 || espaç == 0) {
         window.alert('[ERRO] Reavalie os valores inseridos.')
     } else {
         
@@ -19,13 +19,13 @@ function clicar () {
         ans.innerHTML = '' /* para não aparecer o que estava escrito na tag em html antes de executar a ação E também para poder acrescentar todos os resultados através da concatenação  */
 
         for (var op = inic; op < fim; op+=espaç) {
-            
             ans.innerHTML += `${op} \u{27A1}`   // diferentemente do html, e JS aplica-se \u{INSERIR O CÓDIGO SEM O +U}
         }
         
         for (var op = inic; op > fim; op-=espaç) {
             ans.innerHTML += `${op} \u{27A1}`
         }
+        
         ans.innerHTML += `\u{1F3C1}`
     }
 }
